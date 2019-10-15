b43715e2e5be936fc642e4eca7704259c1fef21c
Sergio Rosa <scarlosjazz@gmail.com>
2019-10-15 13:37:39 -0400

    Category list read delete

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,10 +1,10 @@
 const Category = require('../models/category');
-const slugfy = require('slugify');
+const slugify = require('slugify');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
   const { name } = req.body;
-  let slug = slugfy(name).toLowerCase();
+  let slug = slugify(name).toLowerCase();
 
   let category = new Category({ name, slug });
 
@@ -15,5 +15,44 @@ exports.create = (req, res) => {
       });
     }
     res.json(data);
+  });
+};
+
+exports.list = (req, res) => {
+  Category.find({}).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
+exports.read = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+
+  Category.findOne({ slug }).exec((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(category);
+  });
+};
+
+exports.remove = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+
+  Category.findOneAndRemove({ slug }).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: 'Category deleted successfully',
+    });
   });
 };
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/category');
+const { create, list, read, remove } = require('../controllers/category');
 
 // validators
 const { runValidation } = require('../validators');
@@ -10,10 +10,13 @@ const { requireSignin, adminMiddleware } = require('../controllers/auth');
 router.post(
   '/category',
   categoryCreateValidator,
+  runValidation,
   requireSignin,
   adminMiddleware,
-  runValidation,
   create
 );
+router.get('/categories', list);
+router.get('/category/:slug', read);
+router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
 
 module.exports = router;
