1790d27a1608dbd93657602b6d2c796ccebd207f
Sergio Rosa <scarlosjazz@gmail.com>
2019-10-17 19:34:43 -0400

    Create blog with categories and tags

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -17,15 +17,44 @@ exports.create = (req, res) => {
         error: 'Image could not upload',
       });
     }
+
     const { title, body, categories, tags } = fields;
+
+    if (!title || !title.length) {
+      return res.status(400).json({
+        error: 'title is required',
+      });
+    }
+
+    if (!body || body.length < 200) {
+      return res.status(400).json({
+        error: 'Content is too short',
+      });
+    }
+
+    if (!categories || categories.length === 0) {
+      return res.status(400).json({
+        error: 'At least one category is required',
+      });
+    }
+
+    if (!tags || tags.length === 0) {
+      return res.status(400).json({
+        error: 'At least one tag is required',
+      });
+    }
+
     let blog = new Blog();
-    (blog.title = title),
-      (blog.title = body),
-      (blog.slug = slugify(title).toLowerCase());
-    blog.mtitle = title;
-    blog.mtitle = `${title} | ${process.env.AAP_NAME}`;
-    blog.mdesc = stripHtm(body.substring(0, 160));
+    blog.title = title;
+    blog.body = body;
+    blog.slug = slugify(title).toLowerCase();
+    blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+    blog.mdesc = stripHtml(body.substring(0, 160));
     blog.postedBy = req.user._id;
+
+    // categories and tags
+    let arrayOfCategories = categories && categories.split(',');
+    let arrayOfTags = tags && tags.split(',');
 
     if (files.photo) {
       if (files.photo.size > 10000000) {
@@ -36,14 +65,39 @@ exports.create = (req, res) => {
       blog.photo.data = fs.readFileSync(files.photo.path);
       blog.photo.contentType = files.photo.type;
     }
-    // save in the database
+
     blog.save((err, result) => {
       if (err) {
         return res.status(400).json({
           error: errorHandler(err),
         });
       }
-      res.json(result);
+      // res.json(result);
+      Blog.findByIdAndUpdate(
+        result._id,
+        { $push: { categories: arrayOfCategories } },
+        { new: true }
+      ).exec(err => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        } else {
+          Blog.findByIdAndUpdate(
+            result._id,
+            { $push: { tags: arrayOfTags } },
+            { new: true }
+          ).exec((err, result) => {
+            if (err) {
+              return res.status(400).json({
+                error: errorHandler(err),
+              });
+            } else {
+              res.json(result);
+            }
+          });
+        }
+      });
     });
   });
 };
