4ce076aad5688f19f79989562ad3bf1a6686dcf0
Sergio Rosa <scarlosjazz@gmail.com>
2019-10-27 13:41:28 -0400

    List all blogs

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cf1870cacc82bf431e30";
+/******/ 	var hotCurrentHash = "af718786d6dbeaea3efb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
